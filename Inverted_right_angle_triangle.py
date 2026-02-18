c567a88a1ded7b450f9ec3ef4cbc3548960fcb1e
Dinesh <dineshthirumaran07@gmail.com>
2026-02-18 19:01:33 +0530

    Delete Inverted_right_angle_triangle.py

--- Inverted_right_angle_triangle.py
+++
@@ -1,7 +0,0 @@
-def inverted_right_angle_triangle(n):
-    for row in range (n,0,-1):
-        for col in range(row):
-            print("*",end ="")
-        print()
-
-inverted_right_angle_triangle(6)
