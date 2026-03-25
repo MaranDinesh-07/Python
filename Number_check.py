095335e3e703ca76ae20ace34f4e6f1e51c21a0a
Dinesh <dineshthirumaran07@gmail.com>
2026-03-25 21:35:13 +0530

    Delete Number_check.py

--- Number_check.py
+++
@@ -1,9 +0,0 @@
-# Write a Python Program to Check if a Number is Positive, Negative or Zero
-
-num = float(input("Enter a number: "))
-if num > 0:
-    print("Positive Number")
-elif num == 0:
-    print("The given number is Zero")
-else:
-    print("Negative Number")
