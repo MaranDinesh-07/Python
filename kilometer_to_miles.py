b3284be4e6d2bdb6474e85a577fbdbc09429d0e7
Dinesh <dineshthirumaran07@gmail.com>
2026-03-25 21:36:16 +0530

    Delete kilometer_to_miles.py

--- kilometer_to_miles.py
+++
@@ -1,5 +0,0 @@
-# Write a Python program to convert kilometers to miles
-kilometer = float(input("Enter distance in kilometers:"))
-conversion_factor =  0.621371
-miles = kilometer * conversion_factor
-print(f"{kilometer} kilometer is equal to {miles} miles")
