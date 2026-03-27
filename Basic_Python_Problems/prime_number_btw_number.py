8390e32ab6158afd3e0f395b3ab7b6dcf78553d4
Dinesh <dineshthirumaran07@gmail.com>
2026-03-27 21:05:21 +0530

    Add files via upload
    Work on Day- 3

---
+++ Basic_Python_Problems/prime_number_btw_number.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print(f"The prime number between {num1} and {num2} are:")
+for num in range (num1,num2+1):
+    if num > 1:
+        for i in range(2,num):
+            if num % i == 0:
+                break
+        else:
+            print(num)
---
+++ Basic_Python_Problems/tables.py
@@ -0,0 +1,3 @@
+num = int(input("Enter the number:"))
+for i in range(1,101):
+    print(f"{i} X {num} = {num*i}")
